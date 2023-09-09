28d530146a276f6f5897853f2cc3822b8bd28162
Decodeine <abionaadesola@yahoo.com>
2023-09-09 19:38:20 +0100

    gunicorn

--- Api_endpoint/endpoint.py
+++ Api_endpoint/endpoint.py
@@ -4,6 +4,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Api_endpoint.settings')
 
 application = get_wsgi_application()
-workers = 4  
+workers = 4
 bind = '0.0.0.0:8000'  # Bind to the appropriate host and port
-module = 'endpoint.wsgi:application'  
+module = 'Api_endpoint.endpoint.wsgi:application'
