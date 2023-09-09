da5f2b183a06daa76c43d223cbe675a3f6de7f8e
Decodeine <abionaadesola@yahoo.com>
2023-09-09 19:29:20 +0100

    gunicorn

--- Api_endpoint/endpoint.py
+++ Api_endpoint/endpoint.py
@@ -6,4 +6,4 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'endpoint.settings')
 application = get_wsgi_application()
 workers = 4  
 bind = '0.0.0.0:8000'  # Bind to the appropriate host and port
-module = 'endpoint.wsgi:application'  
+module = 'endpoint.wsgi:endpoint'  
