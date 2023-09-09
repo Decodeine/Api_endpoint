fecf4773d175b0001c27c561fb68368c4b6dcdac
Decodeine <abionaadesola@yahoo.com>
2023-09-09 20:38:31 +0100

    url update

--- Api_endpoint/urls.py
+++ Api_endpoint/urls.py
@@ -21,5 +21,6 @@ from endpoint import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('endpoint.urls')),
+     path('get_info/', views.get_info, name='get_info'),
      
 ]
